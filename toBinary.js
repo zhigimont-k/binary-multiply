3b762c8e6d75376c160c1b97613d8a4aab1a9096
Karina <zhigimont.k@gmail.com>
2017-03-09 22:04:35 +0300

    started working on the table

--- toBinary.js
+++ toBinary.js
@@ -4,8 +4,18 @@
 
 
 function handleButtonClick(){
+    if(!document.getElementById("inputM").value ||
+        !document.getElementById("inputT").value ||
+		!document.getElementById("inputA").value ||
+        !document.getElementById("inputB").value){
+        alert("Please fill in all the text fields!");
+        return 0;
+    }
 	var arrA = document.getElementById("inputA").value;
 	var arrB = document.getElementById("inputB").value;
+	var m = document.getElementById("inputM").value;
+	var t = document.getElementById("inputT").value;
+
 	arrA = arrA.split(" ");
 	if(arrA.length != document.getElementById("inputM").value){
 		alert("Please input m elements!");
@@ -16,10 +26,11 @@ function handleButtonClick(){
 		alert("Please input m elements!");
 		return 0;
 	}
+
 	for (var i = 0; i < arrA.length; i++){
 		arrA[i] = Number(arrA[i]);
 		if (arrA[i] > 15 || arrA[i] < 0){
-			alert("Please inout number in the [0; 15] interval!");
+			alert("Please input number in the [0; 15] interval!");
 		}
 		arrA[i] = decimalToBinary(arrA[i]);
 	}
@@ -30,6 +41,18 @@ function handleButtonClick(){
 		}
 		arrB[i] = decimalToBinary(arrB[i]);
 	}
+
+	var table = document.getElementById("table");
+    var rowIndex = 1;
+    var cellIndex = 0;
+    for (var i = 1; i <= m; i++){
+        rowIndex = i;
+        var row = table.insertRow();
+        var cell = row.insertCell(0);
+        cell.innerHTML = "A"+i+" = "+printBinary(arrA[i-1])+"<br>"+ "B"+i+" = "+printBinary(arrB[i-1])+"<br>";
+    }
+
+
 	
 	
 }
